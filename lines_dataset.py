bd3bb4b8bb763006b4fcec346bae29b95d0b666e
Mehdi Cherti <mehdi@cherti.name>
2014-12-26 20:02:30 +0100

    first commit

---
+++ lines_dataset.py
@@ -0,0 +1,32 @@
+import numpy as np
+import copy
+from theano import config
+import theano.tensor as T
+
+from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix, DefaultViewConverter
+from pylearn2.utils.rng import make_np_rng
+from skimage.draw import line
+
+def gen(number, shape, rng):
+    images = np.zeros( tuple([number] + list(shape)) )
+    x, y = shape[0]/2, shape[1]/2
+    max_r = 0.5*np.sqrt(x**2 + y**2)
+    hidden = []
+    for i in xrange(number):
+        theta = rng.uniform(-np.pi, np.pi)
+        r = rng.uniform(0, max_r)
+        rr, cc = line(x, y, x + int(r * np.cos(theta)), y + int(r * np.sin(theta)))
+        rr = rr[rr < shape[0]]
+        cc = cc[cc < shape[1]]
+        images[i, rr, cc] = 1
+        hidden.append( (theta, r) )
+    hidden  = np.array(hidden)
+    return images.astype(np.float32)
+
+class Lines(DenseDesignMatrix):
+    
+    def __init__(self, shape=20, nb=100):
+        self.shape = shape
+        images = gen(nb, (self.shape, self.shape), rng=np.random)
+        images = images.reshape( (images.shape[0], np.prod(images.shape[1:])) )
+        super(Lines, self).__init__(X=images, view_converter=DefaultViewConverter( (shape, shape, 1)  ) )
